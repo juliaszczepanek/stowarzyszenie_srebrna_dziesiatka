37a7961f810a7e0316fd55a6e44baf8754d08232
juliaszczepanek <juliszczepanek12@gmail.com>
2025-03-28 12:44:37 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# stowarzyszenie_srebrna_dziesiatka
---
+++ srebrna-dziesiatka/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+   <div className='home'>
+        <div className='home__header'>
+            <h1 className='home__heading heading heading--1'>Biegaj z nami w Olkuszu</h1>
+            <h3 className='home__subheading heading heading--2'>Twoje tempo, nasza pasja – spotkajmy się na trasie!</h3>
+        </div>
+        <div className='numbers'>
+            
+            <section className='numbers__section'><h1>16</h1> <p>zorganizowanych biegów</p></section>
+            <section className='numbers__section'><h1>4000+</h1> <p>uczestników</p></section>
+            <section className='numbers__section'><h1>9</h1> <p>lat działalności</p></section>
+            <section className='numbers__section'><h1>3</h1> <p>kategorie biegowe</p></section>
+        </div>
+        <section className='about-us'></section>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.7/gsap.min.js" integrity="sha512-f6bQMg6nkSRw/xfHw5BCbISe/dJjXrVGfz9BSDwhZtiErHwk7ifbmBEtF9vFW8UNIQPhV2uEFVyI/UHob9r7Cw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.7/ScrollTrigger.min.js" integrity="sha512-AcqPGqrrAEtEwe+ADO5R8RbdFi7tuU7b/A2cJJH0Im0D18NRk5p5s4B3E5PMuO81KFw0ClN7J5SHVUJz7KOb0A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://unpkg.com/split-type"></script>
+   </div>
+  
+  )
+}
